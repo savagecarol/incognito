c276eed19fa2b568baa36676bd6efec4a284bc66
AlankarSaxena2712 <saxenaalankar42@gmail.com>
2020-12-04 16:08:27 +0530

    text form rendering

--- incognito/webApp/views.py
+++ incognito/webApp/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
+    if request.method == 'POST':
+        content = request.POST.get('content')
+        print(content)
     return render(request, 'index.html')

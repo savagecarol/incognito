c93cc3bbf94d694bdf17dfe70f385648672e6cfd
leanimax <Vibhuti Singh>
2020-12-04 14:43:18 +0530

    Header working

---
+++ incognito/static/js/script.js
@@ -0,0 +1,18 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+    
+    var view = document.getElementById("quick-links-mobile").style
+    console.log(view)
+
+    if(view.display === "none"){
+        document.getElementById("quick-links-mobile").style.display  = "inline-block";
+        document.getElementById("header").style.height  = "35vh";
+    }
+    else{
+        document.getElementById("quick-links-mobile").style.display  = "none";
+        document.getElementById("header").style.height  = "20vh";
+            
+    }
+  } 
+
+console.log("hello")

f303d45c7053daabd8fdb4acfe9ec5ab9f752bb5
leanimax <vibhutisingh037@gmail.com>
2020-12-05 04:15:11 +0530

    Front End Done

--- static/js/script.js
+++ static/js/script.js
@@ -19,8 +19,14 @@ function changeBackground(){
     document.getElementById("header").style.boxShadow = "0 0.0625rem 0.375rem 0 rgba(0, 0, 0, 0.1)"
 }
 
+function analyzeText(){
+    document.getElementById("loader").style.display = ""
+    sendRequest(document.getElementById('textinput').value, "Text")
+}
+
 function analyzeImage(){
     document.getElementById("loader").style.display = ""
+    document.getElementById("none").style.display = "none"
     imageLoader = document.getElementById("imageLoader")
     const reader = new FileReader()
     reader.readAsDataURL(imageLoader.files[0])
@@ -30,8 +36,7 @@ function analyzeImage(){
         Tesseract.recognize(reader.result).then(function (result){ 
     
             alert(result.text)    
-            document.getElementById("loader").style.display = "none"
-            document.getElementById("image_content").value = result.text;
+            sendRequest(result.text,"Image")
 
         })
     }
@@ -43,6 +48,68 @@ function analyzeImage(){
 
 }
 
+function callbackCheckYesOrNo(response,callBy){
+    console.log(response)
+    var result = "";
+
+    
+    if(response === `[&#x27;Yes&#x27;]`){
+    
+        result = "These Terms And Conditions Sounds Good...</br> The App Can Be Used.."
+
+
+        document.getElementById("loader").style.display = "none"
+        document.getElementById("none").style.display = ""
+        document.getElementById("YesVerified").style.display = ""
+        
+
+        console.log('Yes')
+    }
+    else{
+
+        result = "These Terms And Conditions Does Not Sound.. </br> The App Can't Used.."
+
+
+        document.getElementById("loader").style.display = "none"
+        document.getElementById("none").style.display = ""
+        document.getElementById("NoNotVerified").style.display = ""
+
+        console.log('No')
+    }
+
+    if(callBy === 'Text'){
+        document.getElementById("textUpload").style.display = "none"
+    }
+    else{
+        document.getElementById("imageUpload").style.display = "none"
+    }
+
+    document.getElementById('result-text').innerHTML = result
+    document.getElementById('Result').style.display = ""
+
+    applyColorArray('Result',70)
+}
+
+function sendRequest(value,callBy){
+    var http = new XMLHttpRequest();
+    var url = '/';
+    var returnValue = ""
+    csrfToken = document.getElementsByName("csrfmiddlewaretoken")[0].value
+
+    var params = 'csrfmiddlewaretoken='+csrfToken+'&image_content='+value;
+    http.open('POST', url, true);
+
+    //Send the proper header information along with the request
+    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    http.onreadystatechange = function() {//Call a function when the state changes.
+        if(http.readyState == 4 && http.status == 200) {
+            callbackCheckYesOrNo(http.responseText,callBy)
+        }
+    }
+    http.send(params);
+
+}
 function myFunction(x) {
     x.classList.toggle("change");
     
